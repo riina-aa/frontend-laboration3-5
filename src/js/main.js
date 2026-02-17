617b12b1319a1c9d86ed5c869ade0cfb662c6576
Riina <riina.e.aaltonen@gmail.com>
2026-02-17 17:06:56 +0100

    media querys och mobilmeny klar

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,28 @@
 import '/src/scass/main.scss'
 
+document.addEventListener("DOMContentLoaded", () => {
+    init();
+})
+
+function init() {
+    showSidebar();
+    hideSidebar(); 
+}
+
+function showSidebar() {
+    const menuLi = document.querySelector("#open-menu-li")
+    const sidebar = document.querySelector(".side-menu")
+    
+    menuLi.addEventListener("click", () => {
+        sidebar.style.display = "flex"
+    }); 
+}
+
+function hideSidebar() {
+    const menuLi = document.querySelector("#close-menu-li")
+    const sidebar = document.querySelector(".side-menu")
+    
+    menuLi.addEventListener("click", () => {
+        sidebar.style.display = "none"
+    }); 
+}
