4a878b47be1748ec7e5590328be9d93c25c3f65b
Riina <riina.e.aaltonen@gmail.com>
2026-02-20 13:36:22 +0100

    Developer (#3)
    * Konfiguerat Vite, påbörjat HTML struktur och skapat färgkoder med sass
    
    * installerat sass, påbörjat styling
    
    * klar med header
    
    * Grunden för collage klart
    
    * Mer arbete på collaget
    
    * Flyttade collaget till sidan animation istället
    
    * Fått klart startsidan med bilder, rubrik och lite textkolumner samt footer. Påbörjat undersidan SASS.
    
    * Gjort klart undersidan SASS
    
    * media querys och mobilmeny klar
    
    * Småfix meny och borttagning av länk inför inlämning
    
    * La till active class på li-element
    
    * La till nya menyn i undersidan för animationer
    
    * Animationer klara

--- src/js/main.js
+++ src/js/main.js
@@ -6,23 +6,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function init() {
     showSidebar();
-    hideSidebar(); 
+    hideSidebar();
+    startHeart();
+    startBounce();
 }
 
 function showSidebar() {
     const menuLi = document.querySelector("#open-menu-li")
     const sidebar = document.querySelector(".side-menu")
-    
+
     menuLi.addEventListener("click", () => {
         sidebar.style.display = "flex"
-    }); 
+    });
 }
 
 function hideSidebar() {
     const menuLi = document.querySelector("#close-menu-li")
     const sidebar = document.querySelector(".side-menu")
-    
+
     menuLi.addEventListener("click", () => {
         sidebar.style.display = "none"
-    }); 
+    });
+}
+
+function startHeart() {
+    const loveBtn = document.querySelector("#love-btn");
+    const heart = document.querySelector(".heart")
+
+    loveBtn.addEventListener("click", () => {
+        heart.classList.toggle("animate");
+    })
+}
+
+function startBounce() {
+    const textbox = document.querySelector(".best-test")
+    const bars = document.querySelectorAll(".bar")
+
+    textbox.addEventListener("mouseenter", () => {
+        bars.forEach(bar => {
+            bar.classList.toggle("bar-animation");
+        });
+    });
 }
