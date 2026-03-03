fb1a0e864268e78eba3ed53667838c3cfcc4b3b1
Riina <riina.e.aaltonen@gmail.com>
2026-03-03 14:25:47 +0100

    Lagt till ett stapeldiagram för flest antal sökta kurser

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import '/src/scass/main.scss'
+import Chart from 'chart.js/auto'
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     init();
 })
 
@@ -9,6 +10,7 @@ function init() {
     hideSidebar();
     startHeart();
     startBounce();
+    getData();
 }
 
 function showSidebar() {
@@ -33,6 +35,8 @@ function startHeart() {
     const loveBtn = document.querySelector("#love-btn");
     const heart = document.querySelector(".heart")
 
+    if (!loveBtn || !heart) return;
+
     loveBtn.addEventListener("click", () => {
         heart.classList.toggle("animate");
     })
@@ -42,9 +46,70 @@ function startBounce() {
     const textbox = document.querySelector(".best-test")
     const bars = document.querySelectorAll(".bar")
 
+    if (!textbox || bars.length === 0) return;
+
     textbox.addEventListener("mouseenter", () => {
         bars.forEach(bar => {
             bar.classList.toggle("bar-animation");
         });
     });
 }
+
+let antagningData = [];
+
+async function getData() {
+    const url = "https://mallarmiun.github.io/Frontend-baserad-webbutveckling/Moment%205%20-%20Dynamiska%20webbplatser/statistik_sokande_ht25.json"
+
+    try {
+        const response = await fetch(url);
+        antagningData = await response.json();
+
+        filterData(antagningData);  
+
+    } catch (error) {
+        console.error("Felmeddelande:" + error);
+    }
+}
+
+function filterData(data) {
+
+    let kurser = data.filter(kurs => kurs.type === "Kurs"); 
+    let program = data.filter(program => program.type === "Program"); 
+    
+    kurser.sort((a, b) => b.applicantsTotal - a.applicantsTotal); 
+    program.sort((a, b) => b.applicantsTotal - a.applicantsTotal); 
+
+    let toppKurser = kurser.slice(0, 6); 
+    let toppProgram = program.slice(0, 5);
+    
+    displayKursData(toppKurser); 
+    displayProgramData(toppProgram); 
+}
+
+
+function displayKursData(kurs) {
+
+    new Chart(
+        document.getElementById('stapeldiagram'),
+        {
+            type: 'bar',
+            data: {
+                labels: kurs.map(k => k.name),
+                datasets: [{
+                    label: 'Totalt antal sökande:',
+                    data: kurs.map(k => k.applicantsTotal),
+                    backgroundColor: [
+                        'rgba(255, 99, 132)',
+                        'rgba(255, 159, 64)',
+                        'rgba(255, 205, 86)',
+                        'rgba(75, 192, 192)',
+                        'rgba(54, 162, 235)',
+                        'rgba(153, 102, 255)',
+                        'rgba(201, 203, 207)'
+                    ]
+                }
+                ]
+            }
+        }
+    );
+};
